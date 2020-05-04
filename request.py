0ad38c61e5dc2f72bb8929777351bca4ab4c5198
amith003k <44908284+amith003k@users.noreply.github.com>
2020-05-05 03:23:38 +0530

    request.py added command line args

--- request.py
+++ request.py
@@ -2,13 +2,22 @@ import io
 import numpy as np
 from PIL import Image
 import requests
+import sys
 
-url = 'http://localhost:5000/get_plate'
+def get_plate(img_path='./images/car5.jpg'):
+	url = 'http://35.239.52.119:5000/get_plate'
 
-img = Image.open('./images/car5.jpg')
-buff = io.BytesIO()
-img.save(buff,format='JPEG')
-buff.seek(0)
-response = requests.post(url,data=buff.read())
-print(response._content)
-img.close()
+	img = Image.open(img_path)
+	buff = io.BytesIO()
+	img.save(buff,format='JPEG')
+	buff.seek(0)
+	response = requests.post(url,data=buff.read())
+	print(response._content)
+	img.close()
+
+if __name__=='__main__':
+	if(len(sys.argv)>1):
+		img_path = str(sys.argv[1])
+		get_plate(img_path=img_path)
+	else:
+		get_plate()
